4de2844363c10e6c10dca209c81e89637c02a9ca
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2024-09-23 08:52:04 +0330

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  assetPrefix: '/ehsanahmadpoor-github-io.vercel.app/',
+  assetPrefix: '/ehsanahmadpoor-github-io.vercel.app',
   basePath: '/ehsanahmadpoor-github-io.vercel.app'
 }
